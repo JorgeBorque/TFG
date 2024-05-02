b0cea3af14c7729532e05019c54ba9a296b4537a
JorgeBorque <75611296+JorgeBorque@users.noreply.github.com>
2024-05-02 12:38:15 +0200

    Update chainL_UserA.py

--- chainL_UserA.py
+++ chainL_UserA.py
@@ -201,7 +201,7 @@ async def on_chat_start():
                 cl.input_widget.Select(
                     id="Model",
                     label="Model",
-                    values=["gpt-4", "AI-Growth-Lab_llama-2-7b-clinical-innovation", "mistralai_Mixtral-8x7B-Instruct-v0.1", "meditron-7b", "TheBloke_meditron-7B-GPTQ"],
+                    values=["gpt-4", "AI-Growth-Lab_llama-2-7b-clinical-innovation", "mistralai_Mixtral-8x7B-Instruct-v0.1", "meditron-7b", "TheBloke_meditron-7B-GPTQ", "meta-llama_Meta-Llama-3-8B"],
                     initial_index=1
                 ),
                 cl.input_widget.Select(
